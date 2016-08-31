7359aa6f05a755d6bf249e4307ea013194abc4ad
Becky Brooks <rb9209@ship.edu>
2016-08-31 09:02:35 -0400

    Initial commit of base files for LibGDX project.

---
+++ desktop/src/com/brooks/gdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brooks.gdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brooks.gdx.game.BrooksGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BrooksGdxGame(), config);
+	}
+}
