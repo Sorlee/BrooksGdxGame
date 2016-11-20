0f736e6cdcc90f98f01d2496ab64de15054cb94f
Becky Brooks <ello235@aim.com>
2016-11-20 17:19:24 -0500

    Assignment 12A Added: anim_bunny_copter_01.png, anim_bunny_copter_02.png, anim_bunny_copter_03.png, anim_bunny_copter_04.png, anim_bunny_copter_05.png, anim_bunny_normal_01.png, anim_bunny_normal_02.png, anim_bunny_normal_03.png, anim_gold_coin_01.png, anim_gold_coin_02.png, anim_gold_coin_03.png, anim_gold_coin_04.png, anim_gold_coin_05.png, anim_gold_coin_06.png, brooksgdxgame.pack.atlas, brooksgdxgame.pack.png ChangesMadeTo: Assets.java, AbstractGameObject.java, BunnyHead.java, GoldCoin.java, MenuScreen.java Removed: brooksgdxgame.pack.atlas, brooksgdxgame.pack.png

--- core/src/com/brooks/gdx/game/objects/GoldCoin.java
+++ core/src/com/brooks/gdx/game/objects/GoldCoin.java
@@ -3,6 +3,7 @@ package com.brooks.gdx.game.objects;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.brooks.gdx.game.Assets;
+import com.badlogic.gdx.math.MathUtils;
 
 /**
  * Created by: Becky Brooks
@@ -27,7 +28,8 @@ public class GoldCoin extends AbstractGameObject
 	private void init ()
 	{
 		dimension.set(0.5f, 0.5f);
-		regGoldCoin = Assets.instance.goldCoin.goldCoin;
+		setAnimation(Assets.instance.goldCoin.animGoldCoin);
+		stateTime = MathUtils.random(0.0f, 1.0f);
 		//Set bounding box for collision detection
 		bounds.set(0, 0, dimension.x, dimension.y);
 		collected = false;
@@ -41,7 +43,7 @@ public class GoldCoin extends AbstractGameObject
 		if (collected)
 			return;
 		TextureRegion reg = null;
-		reg = regGoldCoin;
+		reg = animation.getKeyFrame(stateTime, true);
 		batch.draw(reg.getTexture(), position.x, position.y, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionHeight(), reg.getRegionWidth(), false, false);
 	}
 	
